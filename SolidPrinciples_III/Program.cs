fa1a23505c31c55decec6c3bcb11f2f026d40ee3
Surya <surya7ms@gmail.com>
2025-04-27 16:55:35 -0500

    Add project files.

---
+++ SolidPrinciples_III/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples_III
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public interface IWorkable
+    {
+        void Work();
+    }
+
+    public interface IEatable
+    {
+        void Eat();
+    }
+
+    public class Worker : IWorkable, IEatable
+    {
+        public void Work()
+        {
+            Console.WriteLine("Worker is working.");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("Worker is eating.");
+        }
+    }
+
+    public class Robot : IWorkable
+    {
+        public void Work()
+        {
+            Console.WriteLine("Robot is working.");
+        }
+    }
+}
---
+++ SolidPrinciples_III/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SolidPrinciples_III")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SolidPrinciples_III")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5a62fba7-1659-4e22-8fa8-71c6d67d482e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
